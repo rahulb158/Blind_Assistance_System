61ef279cdf8f14cabc017e89373d2cf7f88af5e0
rahulb158 <rahulbajaj158@gmail.com>
2022-04-13 18:12:18 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,153 @@
+from flask import Flask, render_template, request
+import cv2
+import tensorflow
+from keras.models import load_model,Model
+import numpy as np
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.optimizers import Adam
+from keras.layers import Dense, Flatten,Input, Convolution2D, Dropout, LSTM, TimeDistributed, Embedding, Bidirectional, Activation, RepeatVector,Concatenate
+from keras.models import Sequential, Model
+from keras.utils import np_utils
+from keras.preprocessing import image, sequence
+import cv2
+from keras.preprocessing.sequence import pad_sequences
+from tqdm import tqdm
+from tensorflow.keras.applications import ResNet50
+from gtts import gTTS
+import os
+
+
+
+
+vocab = np.load('vocab.npy', allow_pickle=True)
+
+vocab = vocab.item()
+
+inv_vocab = {v:k for k,v in vocab.items()}
+
+
+print("+"*50)
+print("vocabulary loaded")
+
+
+embedding_size = 128
+vocab_size = len(vocab)
+max_len = 31
+
+
+image_model = Sequential()
+
+image_model.add(Dense(embedding_size, input_shape=(2048,), activation='relu'))
+image_model.add(RepeatVector(max_len))
+
+
+language_model = Sequential()
+
+language_model.add(Embedding(input_dim=vocab_size, output_dim=embedding_size, input_length=max_len))
+language_model.add(LSTM(256, return_sequences=True))
+language_model.add(TimeDistributed(Dense(embedding_size)))
+
+
+conca = Concatenate()([image_model.output, language_model.output])
+x = LSTM(128, return_sequences=True)(conca)
+x = LSTM(512, return_sequences=False)(x)
+x = Dense(vocab_size)(x)
+out = Activation('softmax')(x)
+model = Model(inputs=[image_model.input, language_model.input], outputs = out)
+
+model.compile(loss='categorical_crossentropy', optimizer='RMSprop', metrics=['accuracy'])
+
+model.load_weights('mine_model_weights.h5')
+
+print("="*150)
+print("MODEL LOADED")
+
+incept_model = ResNet50(include_top=True)
+last = incept_model.layers[-2].output
+modele = Model(inputs = incept_model.input,outputs = last)
+
+resnet = load_model('model.h5')
+
+print("="*150)
+print("RESNET MODEL LOADED")
+
+
+
+
+app = Flask(__name__)
+
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/after', methods=['GET', 'POST'])
+def after():
+
+    global model, resnet, vocab, inv_vocab, modele
+
+    img = request.files['file1']
+
+    img.save('static/file.jpg')
+
+    print("="*50)
+    print("IMAGE SAVED")
+
+
+    
+    image = cv2.imread('static/file.jpg')
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    image = cv2.resize(image, (224,224))
+
+    image = np.reshape(image, (1,224,224,3))
+
+    
+    
+    incept = modele.predict(image).reshape(1,2048)
+
+    print("="*50)
+    print("Predict Features")
+
+
+    text_in = ['startofseq']
+
+    final = ''
+
+    print("="*50)
+    print("GETING Captions")
+
+    count = 0
+    while tqdm(count < 20):
+
+        count += 1
+
+        encoded = []
+        for i in text_in:
+            encoded.append(vocab[i])
+
+        padded = pad_sequences([encoded], maxlen=max_len, padding='post', truncating='post')
+
+        sampled_index = np.argmax(resnet.predict([incept, padded]))
+
+        sampled_word = inv_vocab[sampled_index]
+
+        if sampled_word != 'endofseq':
+            final = final + ' ' + sampled_word
+
+        text_in.append(sampled_word)
+
+    mytext = final
+    language = 'en'
+    myobj = gTTS(text=mytext, lang=language, slow=False)
+    myobj.save('static/file.mp3')
+    os.system('static/file.mp3')
+
+    return render_template('after.html', data=final)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
---
+++ readme.txt
@@ -0,0 +1,4 @@
+Blind assistance System Using Deep Learning.
+
+Dataset : Flickr8K dataset
+Dataset Link : https://www.kaggle.com/datasets/ming666/flicker8k-dataset 
